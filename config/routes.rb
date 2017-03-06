eddbb74ee42e79e29cd5638a6f197a890698ce8e
Joey Jnr <apeelit@gmail.com>
2017-03-06 01:46:47 -0600

    nexmo integration fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   root to: "pages#index"
   resources :pages
   
-  resources :verifications, only: [:edit, :update]
+  resource :verification, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
