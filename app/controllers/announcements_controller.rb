bfd46f848ec75026d811b09d3542ecbd618d5c30
Joey Jnr <apeelit@gmail.com>
2017-03-08 12:40:43 -0600

    added announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,7 @@
 class AnnouncementsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_announcement, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_verified
   
   def index
     @announcement = Announcements.all.order('created_at DESC')
