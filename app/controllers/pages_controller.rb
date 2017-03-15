253002533715491be330b66efa8814d3ceabf831
Joey Jnr <apeelit@gmail.com>
2017-03-15 13:08:07 -0500

    added announcements to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_verified
   def index
+  	@announcement = Announcement.all.order('created_at DESC')
   end
   
   
