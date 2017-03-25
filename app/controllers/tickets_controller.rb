b5ee14214b68f18b7acb7a065e26ee11838c511c
Joey Jnr <apeelit@gmail.com>
2017-03-25 01:26:42 -0500

    ticket new link fix

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,7 +3,7 @@ class TicketsController < ApplicationController
   before_action :ensure_verified
   before_action :find_ticket, only: [:show, :edit, :update, :destroy]
   def index
-    @ticket = current_user.ticket.all.order('created_at DESC')
+    @tickets = current_user.ticket.all.order('created_at DESC')
   end
   
   def create
