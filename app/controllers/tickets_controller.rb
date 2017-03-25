48a82795a72d051c4637f4d9304f7292fd7964b7
Joey Jnr <apeelit@gmail.com>
2017-03-25 00:30:20 -0500

    ticket new link fix

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,10 +1,23 @@
 class TicketsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_verified
+  before_action :find_ticket, only: [:show, :edit, :update, :destroy]
   def index
     @tickets = current_user.tickets.all.order('created_at DESC')
   end
   
-  def new
+  def create
+    @tickets = Tickets.new(announcements_params)
+
+		if @tickets.save
+			redirect_to @tickets, notice: 'Ticket created'
+		else
+			render "New"
+		end
   end
+  
+  def new
+    @tickets = Tickets.new
+  end
+  
 end
