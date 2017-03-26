54d6a33c2ed870ec60795e5b5453c09b79461ddc
Joey Jnr <apeelit@gmail.com>
2017-03-26 13:23:28 -0500

    general link updates

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,6 +9,7 @@ class TicketsController < ApplicationController
   def create
     @ticket = Ticket.new(tickets_params)
     @ticket.ticket_by = current_user.first_name + " " + current_user.last_name
+    @ticket.save
 		if @ticket.save
 			redirect_to @ticket, notice: 'Ticket created'
 		else
