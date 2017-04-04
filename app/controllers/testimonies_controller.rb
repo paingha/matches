d417192ffc1fbaa48657c789198a5b00fecd969d
Joey Jnr <apeelit@gmail.com>
2017-04-04 15:53:14 -0500

    added testimony controller

--- app/controllers/testimonies_controller.rb
+++ app/controllers/testimonies_controller.rb
@@ -3,15 +3,59 @@ class TestimoniesController < ApplicationController
   before_action :ensure_verified
   
   def index
-    @testimonies = Testimonies.all.order('created_at DESC')
+    @testimony = Testimony.all.order('created_at DESC')
   end
 
-  def show
-  end
+  def create
+    @testimony = Testimony.new(testimonies_params)
 
+		if @testimony.save
+			redirect_to @testimony, notice: 'Testimony created'
+		else
+			render "New"
+		end
+  end
+  
   def new
+    @testimony = Testimony.new
   end
-
+  
+  def update
+    if @testimony.update(testimonies_params)
+			redirect_to @testimony
+		else
+			render "Edit"
+		end
+  end
+  
   def edit
   end
+  
+  def show
+  end
+  
+  def destroy
+    @testimony.destroy
+	redirect_to root_path
+  end
+  
+  def testimonies_params
+		params.require(:testimony).permit(:text)
+	end
+
+	def set_admin
+		if current_user.admin == true
+
+		else
+			
+			redirect_to root_path
+		end
+	end
+
+	
+
+	def find_testimony
+		@testimony = Testimony.find(params[:id])
+	end
+
 end
