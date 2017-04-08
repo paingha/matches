18fd8e98adf8ea479aafc6196102e03802a7f13c
Joey Jnr <apeelit@gmail.com>
2017-04-08 18:11:19 -0500

    removing page load

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_verified
   def index
-  	@announcement = Announcement.all.where(published: 'true').order('created_at DESC')
+  	@announcement = Announcement.where(published: 'true').order('created_at DESC')
   end
   
   
