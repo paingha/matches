9a5fd4bb32b0a60af2bd6df60440aa8d88c74e09
Joey Jnr <apeelit@gmail.com>
2017-04-08 23:18:46 -0500

    fixes generally

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
