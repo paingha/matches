37e4e2a842bab5b5867280e0d2dab1d2c4db60e0
Joey Jnr <apeelit@gmail.com>
2017-04-12 20:42:51 -0500

    authorization fixes'

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,8 +4,8 @@ class TicketsController < ApplicationController
   before_action :find_ticket, only: [:show, :edit, :update, :destroy]
   
   def index
-    if current_user.admin ==true
-      @tickets = tickets.all.order('created_at DESC')
+    if current_user.admin == true
+      @tickets = Tickets.all.order('created_at DESC')
     else
     @tickets = current_user.tickets.all.order('created_at DESC')
     end
