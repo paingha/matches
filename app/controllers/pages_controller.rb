ae316ff00356087620592d8bab4a73158f751112
Joey Jnr <apeelit@gmail.com>
2017-04-13 17:33:17 -0500

    added header partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   before_action :ensure_verified
   def index
   	@announcement = Announcement.where(published: 'true').order('created_at DESC')
+    @match = Match.where(receiver_id: current_user.id)
   end
   
   
